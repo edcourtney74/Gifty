f8da25e1e8257531fe7f7a81d7bfbb1cd5ab9bd3
Ed Courtney <edcourtney74@gmail.com>
2018-11-26 14:12:15 -0500

    Create URL from search page submission

--- assets/javascript/apis.js
+++ assets/javascript/apis.js
@@ -1,3 +1,3 @@
 // Etsy api testing
 
-https://openapi.etsy.com/v2/listings/active?api_key=jydjjl78x1gb73jboqntx9o1&keywords=simpsons
+// https://openapi.etsy.com/v2/listings/active?api_key=jydjjl78x1gb73jboqntx9o1&keywords=simpsons
